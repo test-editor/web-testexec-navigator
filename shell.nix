c389a3f7eab8a8c8528d578ca8e875614ada228f
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-09-21 16:39:12 +0200

    add some nix and direnv files for usage in nix environment

--- shell.nix
+++ shell.nix
@@ -1,14 +1,17 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation {
-    name = "testeditor-java-development";
+    name = "testeditor-angular-development";
     buildInputs = [
         nodejs-9_x
         nodePackages.npm
         nodePackages.yarn
+        nodePackages.jsonlint
+        bashInteractive
     ];
     shellHook = ''
-        npm install @angular/cli
+        # make sure no output is done, since direnv fails with direnv: error unmarshal() base64 decoding: illegal base64 data at input byte ?
+        npm install @angular/cli > /dev/null 2>&1
         export PATH=`pwd`/node_modules/@angular/cli/bin:$PATH
     '';
 }
