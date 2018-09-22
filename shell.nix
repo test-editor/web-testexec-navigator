e59237cc86507fefed94332a8ec93a56174d4caa
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-09-22 15:44:50 +0200

    generalize setting of path in angular project

--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ stdenv.mkDerivation {
     shellHook = ''
         # make sure no output is done, since direnv fails with direnv: error unmarshal() base64 decoding: illegal base64 data at input byte ?
         npm install @angular/cli > /dev/null 2>&1
-        export PATH=`pwd`/node_modules/@angular/cli/bin:$PATH
+        # put all linked executables in node_modules on the path
+        export PATH=`pwd`/node_modules/.bin:$PATH
     '';
 }
