d4c12fb2086e4ba7fa9681de2caf916e4212ca23
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-10-10 17:00:23 +0200

    use new commons version

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "testeditor-angular-development";
     buildInputs = [
-        nodejs-9_x
+        nodejs-10_x
         nodePackages.npm
         nodePackages.yarn
         nodePackages.jsonlint
